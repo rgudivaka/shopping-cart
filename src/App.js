d219f222c2bdbe9d3fcd8ef18e0ef20b45dbe227
Rishi Gudivaka <rishi.gudivaka@gmail.com>
2020-01-15 23:20:07 -0600

    Titles, Descriptions, and Images Working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
 
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('./data/products.json');
+      const response = await fetch("./data/products.json");
       const json = await response.json();
       setData(json);
     };
@@ -13,9 +25,28 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+    <div>
+      <Container>
+        {products.map(product => (
+          <Card key={product.sku}>
+            <CardImg
+              style={{
+                height: "30vh",
+                width: "50vh"
+              }}
+              top
+              src={require("../public/data/products/" + product.sku + "_1.jpg")}
+              alt="Card image cap"
+            />
+            <CardBody>
+              <CardTitle>{product.title}</CardTitle>
+              <CardText>{product.style}</CardText>
+              <Button color="success">Add to Cart</Button>
+            </CardBody>
+          </Card>
+        ))}
+      </Container>
+    </div>
   );
 };
 
